8f919dfe5c944f4a78e936c499cb71a80d92736f
ajk-psu <ajk7267@psu.edu>
2024-11-15 00:37:10 -0500

    Create console_ui.py. Update main.py.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print('Hello world!')
-print('Testing branch.')
+"""
+Program executes here.
+"""
+
+import console_ui
+
+title_text = 'Welcome to the Python Movie Database Manager!'
+"""String: Contains the text used for the user interface's title."""
+
+body_text = 'To start off you can choose to import data from an external CSV file or go straight to the main menu.'
+"""String: Contains the text used for the user interface's body."""
+
+console_ui.create_screen(title_text, body_text)
